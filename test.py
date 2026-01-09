182ce03e0a0179ea45c140c0395ec114bc98fb9a
nampole <nampole@aol.com>
2026-01-09 10:24:57 +0900

    테스트 코드 정리

--- test.py
+++ test.py
@@ -2,10 +2,6 @@
 from scipy.special import gamma, erf, beta 
 import numpy as np
 
-from scipy import ndimage
-import matplotlib.pyplot as plt
-
-
 # 1. 감마 함수 (Gamma function) 예시 ~~.  ~~~
 # gamma(x) = (x-1)! (정수 x > 0)
 print(f"gamma(5) = {gamma(5)}") # 4! = 24
@@ -28,22 +24,3 @@ print(f"1차원 배열: {arr}")
 
 print("Hello, World!")
 print("새로운 세상입니다!")
-
-# 샘플 이미지 로드
-image = plt.imread('your_image.png')
-
-# 가우시안 필터 적용
-filtered_image = ndimage.gaussian_filter(image, sigma=3)
-
-# 원본 이미지와 필터링된 이미지 표시
-plt.figure(figsize=(10, 5))
-
-plt.subplot(1, 2, 1)
-plt.title('Original Image')
-plt.imshow(image, cmap='gray')
-
-plt.subplot(1, 2, 2)
-plt.title('Filtered Image')
-plt.imshow(filtered_image, cmap='gray')
-
-plt.show()
